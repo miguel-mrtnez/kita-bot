56c5c986f47eea272bccad3ec2672747aa9e4473
miguel <martinez.m@uc.cl>
2021-08-17 19:45:01 -0400

    fix(cogs): delete custom help command

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from app.bot import Bot
 
 from cogs.admin import Admin
 from cogs.utils import Utils
-from cogs.help import Help
 
 
 kita = Bot()
 
 kita.add_cog(Admin(kita))
 kita.add_cog(Utils(kita))
-kita.add_cog(Help(kita))
 
 kita.run()
